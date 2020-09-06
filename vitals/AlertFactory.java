0411d562ce335b80dacb988e414adc7f10faa36d
Mohit-it-is <mohitkr.mnnit@gmail.com>
2020-09-07 02:28:39 +0530

    finally

--- vitals/AlertFactory.java
+++ vitals/AlertFactory.java
@@ -1,6 +1,5 @@
 package vitals;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ResourceBundle;
 import vitals.AlertService;
 
@@ -9,19 +8,11 @@ public class AlertFactory {
 		ResourceBundle.clearCache();
 		ResourceBundle rb = ResourceBundle.getBundle("resources/config");
 		String strclass = rb.getString("cname");
-		
 		IReporter ir = null;
 		try {
-<<<<<<< HEAD
-			Class<?> cls = Class.forName(strclass);
-			ir = (IReporter)cls.getDeclaredConstructor().newInstance();
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException 
-				| InvocationTargetException |NoSuchMethodException e) {
-=======
 			Class cls = Class.forName(strclass);
 			ir = (IReporter)cls.newInstance();
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
->>>>>>> cfde567e921bc699af70e14796fb549b658271b2
 			e.printStackTrace();
 		}
 		return ir;
