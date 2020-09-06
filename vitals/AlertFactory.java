7ee1d6298ea37fb970080422d52ea3f5e4f7c65f
Mohit-it-is <68475914+Mohit-it-is@users.noreply.github.com>
2020-09-06 23:38:33 +0530

    Update AlertFactory.java

--- vitals/AlertFactory.java
+++ vitals/AlertFactory.java
@@ -7,12 +7,12 @@ import java.util.ResourceBundle;
 public class AlertFactory {
 	public static IReporter getAlertInstance() {
 		ResourceBundle.clearCache();
-		ResourceBundle rb = ResourceBundle.getBundle("properties/config");
+		ResourceBundle rb = ResourceBundle.getBundle("vitals/properties/config");
 		String strclass = rb.getString("cname");
 		IReporter ir = null;
 		try {
 			Class cls = Class.forName(strclass);
-			ir = (IReporter)cls.getDeclaredConstructor().newInstance();
+			ir = (IReporter)cls.newInstance();
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException 
 				| InvocationTargetException |NoSuchMethodException e) {
 			e.printStackTrace();
